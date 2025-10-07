858ccc4032e7ae3aa20f484576aa1c162d11196c
Dylan Llewellyn <46717769+herefishyfish@users.noreply.github.com>
2025-10-07 13:27:16 +0800

    feat: register SquircleContentView component in app initialization

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,9 @@ import { createApp, registerElement } from 'nativescript-vue';
 import '@nativescript/canvas-polyfill';
 import Home from './components/Home.vue';
 import { BlurView } from "@nativescript-community/ui-blurview";
+import { SquircleContentView } from './components/squircle';
 
 registerElement("BlurView", () => BlurView);
+registerElement("SquircleContentView", () => SquircleContentView);
 
 createApp(Home).start();
